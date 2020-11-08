0ec53d65bf6844184e5e565f34bc4bed4e915871
coord.e <me@coord-e.com>
2020-11-08 07:12:16 +0000

    Add: Prettify `Message`

--- src/model/kaisanee.rs
+++ src/model/kaisanee.rs
@@ -1,4 +1,6 @@
-use serenity::model::id::UserId;
+use std::fmt::{self, Display};
+
+use serenity::model::{id::UserId, misc::Mentionable};
 
 #[derive(Debug, Clone)]
 pub enum KaisaneeSpecifier {
@@ -10,5 +12,25 @@ pub enum KaisaneeSpecifier {
 impl Default for KaisaneeSpecifier {
     fn default() -> Self {
         KaisaneeSpecifier::All
+    }
+}
+
+impl Display for KaisaneeSpecifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            KaisaneeSpecifier::Me => f.write_str("あなた"),
+            KaisaneeSpecifier::All => f.write_str("全員"),
+            KaisaneeSpecifier::Users(ids) => {
+                if let Some(id) = ids.first() {
+                    write!(f, "{}", id.mention())?;
+                }
+
+                for id in ids[1..].into_iter() {
+                    write!(f, ", {}", id.mention())?;
+                }
+
+                Ok(())
+            }
+        }
     }
 }
--- src/model/message.rs
+++ src/model/message.rs
@@ -3,7 +3,7 @@ use std::fmt::{self, Display};
 use crate::error::Error;
 use crate::model::kaisanee::KaisaneeSpecifier;
 
-use chrono::{DateTime, FixedOffset};
+use chrono::{DateTime, Datelike, FixedOffset, Timelike, Utc};
 use serenity::model::{id::UserId, misc::Mentionable};
 
 pub enum Message {
@@ -29,12 +29,14 @@ impl Display for Message {
 ",
             ),
             Message::ScheduledAt(kaisanee, time) => {
-                // let now = Utc::now().with_timezone(&time.timezone());
-                write!(f, "{}に{:?}を解散します", time, kaisanee)
+                let now = Utc::now().with_timezone(&time.timezone());
+                fmt_datetime_when(f, *time, now)?;
+                write!(f, "に{}を解散します", kaisanee)
             }
             Message::ScheduledBy(kaisanee, time) => {
-                // let now = Utc::now().with_timezone(&time.timezone());
-                write!(f, "{}までに{:?}を解散します", time, kaisanee)
+                let now = Utc::now().with_timezone(&time.timezone());
+                fmt_datetime_when(f, *time, now)?;
+                write!(f, "までに{}を解散します", kaisanee)
             }
             Message::Kaisan(ids) => {
                 for id in ids {
@@ -42,8 +44,43 @@ impl Display for Message {
                 }
                 f.write_str("解散！")
             }
-            Message::HandleError(e) => e.fmt(f),
-            Message::KaisanError(e) => write!(f, "解散できませんでした: {}", e),
+            Message::HandleError(e) => fmt_error(f, e),
+            Message::KaisanError(e) => {
+                f.write_str("解散できませんでした: ")?;
+                fmt_error(f, e)
+            }
         }
     }
+}
+
+fn fmt_error(f: &mut fmt::Formatter, e: &Error) -> fmt::Result {
+    match e {
+        Error::NotInVoiceChannel => f.write_str("ボイスチャンネルに入った状態で使ってほしい"),
+        Error::InvalidCommand(_) => f.write_str("コマンドがわからない"),
+        Error::UnreachableTime { .. } => f.write_str("過去を変えることはできない"),
+        _ => f.write_str("ダメそう"),
+    }
+}
+
+fn fmt_datetime_when(
+    f: &mut fmt::Formatter,
+    time: DateTime<FixedOffset>,
+    now: DateTime<FixedOffset>,
+) -> fmt::Result {
+    if time.date() != now.date() {
+        write!(f, "{}/{} ", time.date().month(), time.date().day())?;
+    }
+    if time.hour() != now.hour() {
+        write!(f, "{}時", time.hour())?;
+    }
+    write!(f, "{}分（", time.minute())?;
+
+    let duration = time - now;
+    if duration.num_hours() != 0 {
+        write!(f, "{}時間", duration.num_hours())?;
+    }
+    if duration.num_minutes() != 0 || (duration.num_hours() == 0 && duration.num_days() == 0) {
+        write!(f, "{}分", duration.num_minutes() % 60)?;
+    }
+    write!(f, "後）")
 }
